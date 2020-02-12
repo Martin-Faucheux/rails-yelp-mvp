55a9796b82a42d0f982a0093356eea1720533cfa
Martin Faucheux <martin.faucheux@edhec.com>
2020-02-12 15:16:40 +0100

    controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    category:      'french',
+    phone_number:  '08767267367',
+    address: 'Paris 12'
+  },
+  {
+    name:         'Pizza East',
+    category:      'italian',
+    phone_number:  '0837383392',
+    address: 'paris 18'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
